5385525d686816ebc69f9996964e4c5579729234
Paracosm-Daemon <paracosmdaemon@gmail.com>
2024-10-26 15:45:12 -0700

    Cleanup hoverProvider

--- src/hoverProvider.ts
+++ src/hoverProvider.ts
@@ -6,7 +6,7 @@ import * as vscode from "vscode";
 import { PowerlangProvider } from "./powerlangProvider";
 import { PowerlangHandle } from "./powerlangHandle";
 
-import { REGEX_BREAK, REGEX_PATH_BREAK } from "./parser/powerlangCore";
+import { FLAG_ANNOTATION, REGEX_BREAK, REGEX_PATH_BREAK } from "./parser/powerlangCore";
 // #endregion
 // #region Classes
 export class PowerlangHoverProvider extends PowerlangProvider
@@ -21,32 +21,65 @@ export class PowerlangHoverProvider extends PowerlangProvider
 	// #region Private
 	private _registerProviders(): void
 	{
+		const provider: PowerlangHoverProvider = this;
 		this.handle.context.subscriptions.push(vscode.languages.registerHoverProvider("powerlang", {
-			provideHover(document: vscode.TextDocument, position: vscode.Position, _cancel: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover | undefined>
+			provideHover(document: vscode.TextDocument, position: vscode.Position, cancel: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover>
 			{
 				const lineText: string = document.lineAt(position).text;
 
 				const cursorPosition: number = position.character;
 				const cursorRange: string = lineText.substring(0, cursorPosition);
 				// Find the latest declaration break so that we can autocomplete assignments/conditionals
-				REGEX_PATH_BREAK.lastIndex = 0;
-
 				let breakMatch: RegExpExecArray | null;
 				let lastBreak: number = -1;
 
-				while ((breakMatch = REGEX_PATH_BREAK.exec(cursorRange)) !== null) lastBreak = breakMatch.index + breakMatch[ 0 ].length;
+				REGEX_BREAK.lastIndex = 0;
+				// Search for flags first
+				// #region Flags
+				while ((breakMatch = REGEX_BREAK.exec(cursorRange)) !== null && !cancel.isCancellationRequested) lastBreak = breakMatch.index + breakMatch[ 0 ].length;
+				if (cancel.isCancellationRequested) return undefined;
 
-				const postRange: string = lastBreak < 0 ? lineText : lineText.slice(lastBreak);
-				const pathBreak: number = postRange.search(REGEX_PATH_BREAK);
+				const flagBegin: string = lastBreak < 0 ? lineText : lineText.slice(lastBreak);
 
-				const currentRange: string = pathBreak < 0 ? postRange : postRange.substring(0, pathBreak);
-				if (cursorRange.startsWith("@flag"))
+				const termEnd: number = flagBegin.search(REGEX_BREAK);
+				const flagFull: string = termEnd < 0 ? flagBegin : flagBegin.substring(0, termEnd);
+
+				const upperFlag: string = flagFull.toUpperCase();
+				const flagIndex: number = upperFlag.indexOf(FLAG_ANNOTATION);
+
+				if (flagIndex >= 0 && upperFlag.trimStart().startsWith(FLAG_ANNOTATION))
 				{
-					console.warn("THIS IS A FLAG");
-					return new vscode.Hover(`@flag ${currentRange}`)
-				}
-				console.log(currentRange);
+					const flagOffset: number = Math.max(0, lastBreak);
+					const flagName: string = flagFull.slice(FLAG_ANNOTATION.length).trimStart();
 
+					const flagRange: vscode.Range = new vscode.Range(position.line, flagOffset + flagIndex, position.line, flagOffset + flagFull.length);
+					const flagTitle: vscode.MarkdownString = new vscode.MarkdownString(`\`\`\`\n@flag ${flagName}\n\`\`\``);
+
+					if (!(flagName in provider.handle.flagAnnotations))
+						return new vscode.Hover(
+							flagTitle,
+							flagRange
+						);
+
+					return new vscode.Hover(
+						[
+							flagTitle,
+							new vscode.MarkdownString(provider.handle.flagAnnotations[ flagName ])
+						],
+						flagRange
+					);
+				}
+				// #endregion
+				// #region Variables
+				// let pathMatch: RegExpExecArray | null;
+				// let pathBreak: number = -1;
+
+				// REGEX_PATH_BREAK.lastIndex = 0;
+				// while ((pathMatch = REGEX_PATH_BREAK.exec(cursorRange)) !== null) pathBreak = pathMatch.index + pathMatch[ 0 ].length;
+
+				// const postRange: string = pathBreak < 0 ? lineText : lineText.slice(pathBreak);
+				// const pathStart: number = postRange.search(REGEX_PATH_BREAK);
+				// #endregion
 				return undefined;
 			}
 		}));
