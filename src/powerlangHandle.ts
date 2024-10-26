ade9cc86e24c52ffbe35c65d69e9b6d15418601a
Paracosm-Daemon <paracosmdaemon@gmail.com>
2024-10-26 00:18:47 -0700

    Add globalsRegenerated event

--- src/powerlangHandle.ts
+++ src/powerlangHandle.ts
@@ -1,13 +1,26 @@
 "use strict";
 // #region Imports
 import * as vscode from "vscode";
-
+import * as path from "path";
+import * as fs from "fs";
+// #endregion
+// #region Exports
+export const FILE_ENCODING: BufferEncoding & fs.WriteFileOptions = "utf-8";
+// #endregion
+// #region Modules
 import { PowerlangCompletionProvider } from "./completionProvider";
+import { PowerlangAPI, PowerlangGlobal } from "./powerlangAPI";
 import { PowerlangColorProvider } from "./colorProvider";
 import { PowerlangScraper } from "./powerlangScraper";
 import { PowerlangParser } from "./powerlangParser";
 
 import { INTERNAL_NAME } from "./extension";
+// #endregion
+// #region Types
+export type RegeneratedEventParams = {
+	libraries: PowerlangGlobal[],
+	globals: PowerlangAPI[]
+}
 // #endregion
 // #region Constants
 const VERSION_KEY: string = `${INTERNAL_NAME}-version`;
@@ -15,10 +28,17 @@ const VERSION_KEY: string = `${INTERNAL_NAME}-version`;
 // #region Classes
 export class PowerlangHandle
 {
+	// #region Variables
+	public readonly globalsRegeneratedEmitter: vscode.EventEmitter<RegeneratedEventParams>;
+	public readonly globalsRegenerated: vscode.Event<RegeneratedEventParams>;
+	// #endregion
 	// #region Functions
 	// #region Public
 	public constructor(public readonly context: vscode.ExtensionContext)
 	{
+		this.globalsRegeneratedEmitter = new vscode.EventEmitter();
+		this.globalsRegenerated = this.globalsRegeneratedEmitter.event;
+
 		new PowerlangParser(this);
 		new PowerlangScraper(this);
 
@@ -28,6 +48,7 @@ export class PowerlangHandle
 		this._initialize();
 	}
 
+	public getResourcePath(fileName: string): string { return this.context.extensionPath + path.sep + "resources" + path.sep + fileName; }
 	public formatNotification(message: string): string { return `${INTERNAL_NAME}: ${message}`; }
 
 	public showInformationMessage(message: string, ...items: string[]): Thenable<string | undefined> { return vscode.window.showInformationMessage(this.formatNotification(message), ...items); }
